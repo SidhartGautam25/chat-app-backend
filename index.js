fcf483f0999ba8622ead7dfe53258510a744d62c
sidhart25 <navneetnishchal1420@gmail.com>
2023-11-21 01:36:22 +0530

    luck

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-kappa-blush.vercel.app/",
+    origin: "https://chat-app-kappa-blush.vercel.app",
     methods: ["GET", "POST"],
   },
 });
